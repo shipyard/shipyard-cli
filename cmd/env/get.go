a3663d9fe5bd46ec73c6e18b5a8b14ccc0909ae5
Max Sokolovsky <genexpr@protonmail.com>
2022-10-20 16:13:37 -0400

    Adjust flag names, make 'json' flag work properly on all env commands

--- cmd/env/get.go
+++ cmd/env/get.go
@@ -21,6 +21,12 @@ func NewGetEnvironmentCmd() *cobra.Command {
 		Aliases:      []string{"env"},
 		SilenceUsage: true,
 		Short:        "Get environment by ID",
+		// Due to an issue in viper, bind the 'json' flag in PreRun for each command that uses
+		// a flag name already bound to a sibling command.
+		// See https://github.com/spf13/viper/issues/233#issuecomment-386791444
+		PreRun: func(cmd *cobra.Command, args []string) {
+			viper.BindPFlag("json", cmd.Flags().Lookup("json"))
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if len(args) > 0 {
 				return getEnvironmentByID(args[0])
@@ -30,7 +36,6 @@ func NewGetEnvironmentCmd() *cobra.Command {
 	}
 
 	cmd.Flags().Bool("json", false, "JSON output")
-	viper.BindPFlag("json", cmd.Flags().Lookup("json"))
 
 	return cmd
 }
@@ -41,6 +46,9 @@ func NewGetAllEnvironmentsCmd() *cobra.Command {
 		Aliases:      []string{"envs"},
 		SilenceUsage: true,
 		Short:        "Get all environments",
+		PreRun: func(cmd *cobra.Command, args []string) {
+			viper.BindPFlag("json", cmd.Flags().Lookup("json"))
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return getAllEnvironments()
 		},
@@ -49,17 +57,17 @@ func NewGetAllEnvironmentsCmd() *cobra.Command {
 	cmd.Flags().String("name", "", "Filter by name")
 	viper.BindPFlag("name", cmd.Flags().Lookup("name"))
 
-	cmd.Flags().String("org_name", "", "Filter by org name")
-	viper.BindPFlag("org_name", cmd.Flags().Lookup("org_name"))
+	cmd.Flags().String("org-name", "", "Filter by org name")
+	viper.BindPFlag("org-name", cmd.Flags().Lookup("org-name"))
 
-	cmd.Flags().String("repo_name", "", "Filter by repo name")
-	viper.BindPFlag("repo_name", cmd.Flags().Lookup("repo_name"))
+	cmd.Flags().String("repo-name", "", "Filter by repo name")
+	viper.BindPFlag("repo-name", cmd.Flags().Lookup("repo-name"))
 
 	cmd.Flags().String("branch", "", "Filter by branch")
 	viper.BindPFlag("branch", cmd.Flags().Lookup("branch"))
 
-	cmd.Flags().String("pull_request_number", "", "Filter by pull request number")
-	viper.BindPFlag("pull_request_number", cmd.Flags().Lookup("pull_request_number"))
+	cmd.Flags().String("pull-request-number", "", "Filter by pull request number")
+	viper.BindPFlag("pull-request-number", cmd.Flags().Lookup("pull-request-number"))
 
 	cmd.Flags().Bool("deleted", false, "Filter by deleted")
 	viper.BindPFlag("deleted", cmd.Flags().Lookup("deleted"))
@@ -67,11 +75,10 @@ func NewGetAllEnvironmentsCmd() *cobra.Command {
 	cmd.Flags().Int("page", 0, "Page number requested")
 	viper.BindPFlag("page", cmd.Flags().Lookup("page"))
 
-	cmd.Flags().Int("page_size", 0, "Page size requested")
-	viper.BindPFlag("page_size", cmd.Flags().Lookup("page_size"))
+	cmd.Flags().Int("page-size", 0, "Page size requested")
+	viper.BindPFlag("page-size", cmd.Flags().Lookup("page-size"))
 
 	cmd.Flags().Bool("json", false, "JSON output")
-	viper.BindPFlag("json", cmd.Flags().Lookup("json"))
 
 	return cmd
 }
@@ -87,16 +94,16 @@ func getAllEnvironments() error {
 	if name := viper.GetString("name"); name != "" {
 		params["name"] = name
 	}
-	if orgName := viper.GetString("org_name"); orgName != "" {
+	if orgName := viper.GetString("org-name"); orgName != "" {
 		params["org_name"] = orgName
 	}
-	if repoName := viper.GetString("repo_name"); repoName != "" {
+	if repoName := viper.GetString("repo-name"); repoName != "" {
 		params["repo_name"] = repoName
 	}
 	if branch := viper.GetString("branch"); branch != "" {
 		params["branch"] = branch
 	}
-	if pullRequestNumber := viper.GetString("pull_request_number"); pullRequestNumber != "" {
+	if pullRequestNumber := viper.GetString("pull-request-number"); pullRequestNumber != "" {
 		params["pull_request_number"] = pullRequestNumber
 	}
 	if deleted := viper.GetBool("deleted"); deleted {
@@ -105,7 +112,7 @@ func getAllEnvironments() error {
 	if page := viper.GetInt("page"); page != 0 {
 		params["page"] = strconv.Itoa(page)
 	}
-	if pageSize := viper.GetInt("page_size"); pageSize != 0 {
+	if pageSize := viper.GetInt("page-size"); pageSize != 0 {
 		params["pageSize"] = strconv.Itoa(pageSize)
 	}
 
@@ -203,32 +210,27 @@ func unmarshalManyEnv(p []byte) (respManyEnvs, error) {
 	return r, err
 }
 
+type environment struct {
+	Attributes struct {
+		Name     string `json:"name"`
+		URL      string `json:"url"`
+		Projects []struct {
+			PullRequestNumber int    `json:"pull_request_number"`
+			RepoName          string `json:"repo_name"`
+		} `json:"projects"`
+	} `json:"attributes"`
+
+	ID string `json:"id"`
+}
+
 type respOneEnv struct {
 	Data struct {
-		Attributes struct {
-			Name     string `json:"name"`
-			URL      string `json:"url"`
-			Projects []struct {
-				PullRequestNumber int    `json:"pull_request_number"`
-				RepoName          string `json:"repo_name"`
-			} `json:"projects"`
-		} `json:"attributes"`
-
-		ID string `json:"id"`
+		environment
 	} `json:"data"`
 }
 
 type respManyEnvs struct {
 	Data []struct {
-		Attributes struct {
-			Name     string `json:"name"`
-			URL      string `json:"url"`
-			Projects []struct {
-				PullRequestNumber int    `json:"pull_request_number"`
-				RepoName          string `json:"repo_name"`
-			} `json:"projects"`
-		} `json:"attributes"`
-
-		ID string `json:"id"`
+		environment
 	} `json:"data"`
 }
