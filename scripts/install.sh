141568ea4a63bae69256aa63d4d2450173e7b981
Akshay Kalia <akshay@pillar.tech>
2022-11-29 10:14:49 -0500

    chore: installation script

---
+++ scripts/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+RELEASES_URL="https://github.com/shipyardbuild/shipyard-cli/releases"
+
+last_version() {
+	curl --silent --location --fail \
+        --output /dev/null --write-out %{url_effective} ${RELEASES_URL}/latest |
+		grep -Eo '[0-9]+\.[0-9]+\.[0-9]+$'
+}
+
+main() {
+	default_dir=/usr/local/bin
+
+    case $(uname -m) in
+        i386 | i686)    ARCH="386" ;;
+        x86_64)         ARCH="amd64" ;;
+        arm64)          ARCH="arm64" ;;
+    esac
+
+	VERSION="$(last_version)"
+	
+    URL="${RELEASES_URL}/download/v${VERSION}/shipyard-$(uname -s)-${ARCH}"
+    
+    curl --silent --output "${default_dir}/shipyard"  --fail "$URL"
+    chmod +x ${default_dir}/shipyard
+}
+
+main "$@"
