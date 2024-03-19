57b417250dee898def8d662450c7de3bcff05016
Max Sokolovsky <genexpr@protonmail.com>
2024-03-19 13:04:45 -0400

    Add support for telepresence

--- commands/telepresence/connect.go
+++ commands/telepresence/connect.go
@@ -1,56 +1,51 @@
 package telepresence
 
 import (
-	"errors"
 	"fmt"
 	"os/exec"
 
-	"github.com/shipyard/shipyard-cli/pkg/client"
-	"github.com/shipyard/shipyard-cli/pkg/k8s"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+
+	"github.com/shipyard/shipyard-cli/pkg/client"
+	"github.com/shipyard/shipyard-cli/pkg/display"
+	"github.com/shipyard/shipyard-cli/pkg/k8s"
 )
 
-var errNoEnvironment = errors.New("environment ID argument not provided")
+func NewTelepresenceCmd(c client.Client) *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "telepresence",
+	}
+	cmd.AddCommand(NewConnectCmd(c))
+	return cmd
+}
 
 func NewConnectCmd(c client.Client) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "telepresence connect [environment ID]",
-		Aliases: []string{"env"},
-		Short:   "Connect telepresence to an environment by ID",
+		Use:     "connect",
+		Aliases: []string{"c"},
+		Short:   "Connect to an environment via telepresence",
 		Example: `  # Connect telepresence to env ID 12345:
-  shipyard telepresence connect 12345
-  
-  # Get all the details for environment ID 12345 in JSON format:
-  shipyard get environment 12345 --json`,
+  shipyard telepresence connect 12345`,
 		SilenceUsage: true,
-		// Due to an issue in viper, bind the 'json' flag in PreRun for each command that uses
-		// a flag name already bound to a sibling command.
-		// See https://github.com/spf13/viper/issues/233#issuecomment-386791444
 		PreRun: func(cmd *cobra.Command, args []string) {
 			_ = viper.BindPFlag("env", cmd.Flags().Lookup("env"))
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if len(args) > 0 {
-				return connect(c)
-			}
-			return errNoEnvironment
+			return connect(c)
 		},
 	}
-
-	cmd.Flags().String("env", "", "Environment ID")
+	cmd.Flags().String("env", "", "environment ID")
 	_ = cmd.MarkFlagRequired("env")
-
 	return cmd
 }
 
 func connect(c client.Client) error {
 	if _, err := exec.LookPath("telepresence"); err != nil {
-		return fmt.Errorf("telepresence not found, please make sure telepresence is in your path")
+		return fmt.Errorf("telepresence not found, please make sure it's in your PATH")
 	}
 
 	id := viper.GetString("env")
-
 	k, err := k8s.NewConfig(c, id)
 	if err != nil {
 		return err
@@ -66,10 +61,8 @@ func connect(c client.Client) error {
 	).Output()
 
 	if err != nil {
-		fmt.Println(string(out))
-		fmt.Println(err)
+		display.Println(string(out))
 		return err
 	}
-
 	return nil
 }
