d6827e03ff40f80ae48d7d27bc02dc60367136a3
Akshay K <akshay@shipyard.build>
2025-07-18 15:01:34 -0400

    feat: add update command (#45)

--- commands/root.go
+++ commands/root.go
@@ -77,6 +77,7 @@ func setupCommands() {
 	rootCmd.AddCommand(NewLoginCmd())
 	rootCmd.AddCommand(NewGetCmd(c))
 	rootCmd.AddCommand(NewSetCmd())
+	rootCmd.AddCommand(NewUpdateCmd())
 	rootCmd.AddCommand(volumes.NewResetCmd(c))
 	rootCmd.AddCommand(volumes.NewCreateCmd(c))
 	rootCmd.AddCommand(volumes.NewUploadCmd(c))
